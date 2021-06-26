300004f1dd5a0296f283a6798f5cbdfb5d350362
Vishal Maurya <Vishal_Maurya@spe.sony.com>
2021-06-26 18:17:07 +0530

    few changes

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,9 @@ const Home = () => {
     return (
         <div className="home">
             <h4>Welcome to MYAPP</h4>
+            <p> Username: Vishal</p>
+            <p> password: test1234</p>
+
         </div>
     )
 }
